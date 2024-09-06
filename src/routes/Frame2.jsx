322501a232e9d02cba73feae9054daa5418d6dde
Khush Desai <114131222+It-is-KD@users.noreply.github.com>
2024-09-06 12:11:07 +0530

    Delete duplicate src directory

--- src/routes/Frame2.jsx
+++
@@ -1,9 +0,0 @@
-import Frame2 from "../components/Frame2"
-
-const Frame2 = () => {
-  return (
-    <Frame2/>
-  )
-}
-
-export default Frame2
--- src/routes/Upload.jsx
+++
@@ -1,9 +0,0 @@
-import UploadComponent from '../components/UploadComponent';
-
-function Upload() {
-  return(
-    <UploadComponent/>
-  )
-}
-
-export default Upload;
